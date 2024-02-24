4fd9a7fb4167e6ee798b8b42d97e61fcf1862dd9
Michanco <michanco80@gmail.com>
2024-02-24 14:59:58 -0800

    HW 24.02.24_2

--- HW1_UIKit/PhotoModel.swift
+++ HW1_UIKit/PhotoModel.swift
@@ -8,15 +8,26 @@
 import Foundation
 
 struct PhotoModel: Decodable{
+    var response: Photos
+}
+
+struct Photos: Decodable{
     var items: [Photo]
 }
 
 struct Photo: Decodable{
     var id: Int
     var text: String?
+    var sizes: [Size]
     
     enum CodingKeys: String, CodingKey{
         case id
         case text
+        case sizes
     }
 }
+
+struct Size: Codable{
+    var url: String
+}
+
